49255010c31c2102e5efe13e68ce9ecfa89c342f
TepparitRattanachai <67030297@kmitl.ac.th>
2024-11-21 05:50:11 +0000

    mult.py

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,12 @@
+x = fioat(input("Enter the firet number:"))
+print(x)
+ัy = fioat(input(input"Enter the second number:"))
+print(y)
+z = x*y
+print(x, "x",y,"=",z)
+if z> 0 :
+    print("The result is positive")
+    else z> 0 :
+        print("The result is negative")
+else:
+    print("the result is positive and negative.")
