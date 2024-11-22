4e517e98a80391e56fd813283fd78d5dee20b630
TepparitRattanachai <67030297@kmitl.ac.th>
2024-11-22 03:01:23 +0000

    calculaor.py

---
+++ cell04/ex02/calculaor.py
@@ -0,0 +1,7 @@
+A = int(input("Give me the first number : "))
+B = int(input("Give me the second number : "))
+Print("Thank you")
+print(f"{A} + {B} = {A+b}")
+print(f"{A} - {B} = {A-b}")
+print(f"{A} * {B} = {A*b}")
+print(f"{A} / {B} = {A/b}")
