09a55c30faef07201d9ecc8532a12a71ab96bbde
TepparitRattanachai <67030297@kmitl.ac.th>
2024-11-22 07:19:43 +0000

    calculaor.py

--- cell04/ex01/age.py
+++ cell04/ex01/age.py
@@ -1,5 +1,8 @@
 age = int(input("please tell me your age: "))
-print(f"You") are currently {age} years old.)
-print(f"In 10 years, you'11 be {age + 10} years old.")
-print(f"In 20 years, you'11 be {age + 10} years old.")
-print(f"In 10 years, you'11 be {age + 10} years old.")
+print ("You are currently",age,"years old.")
+ageone = age + 10
+agetwo = age + 20
+agethree = age + 30
+print("In 10 years,you '11 be {ageone}years old.")
+print("In 20 years,you '11 be {agetwo}years old.")
+print("In 30 years,you '11 be {agethree}years old.")
--- cell04/ex02/calculaor.py
+++ cell04/ex02/calculaor.py
@@ -1,7 +1,11 @@
-A = int(input("Give me the first number : "))
-B = int(input("Give me the second number : "))
-Print("Thank you")
-print(f"{A} + {B} = {A+b}")
-print(f"{A} - {B} = {A-b}")
-print(f"{A} * {B} = {A*b}")
-print(f"{A} / {B} = {A/b}")
+x = int(input("Give me the first number:"))
+ัy = int(input("Give me the second number"))
+print("Thank you!")
+บวก = x+y
+ลบ = x-y
+หาร = x/y
+คูณ = x*y
+print(x,"+",y,"=",บวก)
+print(x,"-",y,"=",ลบ)
+print(x,"/",y,"=",หาร)
+print(x,"*",y,"=",คูณ)
