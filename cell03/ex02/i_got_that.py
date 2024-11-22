101fa4306e88ddfb6d80e20dbc1699cef8f0aad1
TepparitRattanachai <67030297@kmitl.ac.th>
2024-11-22 02:55:32 +0000

    i_got_that.py.

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,7 @@
+whileTrue:
+    a = input("what you gotta say? : ")
+    if a == "STOP":
+        print("")
+        break
+    else:
+        print(f"I got that! Anything else? : {a}")
---
+++ cell04/ex01/age.py
@@ -0,0 +1,5 @@
+age = int(input("please tell me your age: "))
+print(f"You") are currently {age} years old.)
+print(f"In 10 years, you'11 be {age + 10} years old.")
+print(f"In 20 years, you'11 be {age + 10} years old.")
+print(f"In 10 years, you'11 be {age + 10} years old.")
